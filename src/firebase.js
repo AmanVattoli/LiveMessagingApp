0a8d3b10701241cc4769727af9b3aede757cb77d
AmanVattoli <usiriux@gmail.com>
2023-11-11 00:59:11 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseApp = firebase.initializeApp({
-    //firebase config goes here
+    apiKey: "AIzaSyBR4-_UWkPKlyRiFaA7MfEMCrIjufe1gF0",
+    authDomain: "chatapp-24a8c.firebaseapp.com",
+    projectId: "chatapp-24a8c",
+    storageBucket: "chatapp-24a8c.appspot.com",
+    messagingSenderId: "127791154895",
+    appId: "1:127791154895:web:96eb7075e4b73ccd45d9ba",
+    measurementId: "G-39JLZCQDRD" 
 })
 
 const db = firebaseApp.firestore()
